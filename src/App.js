3f5bc2bab4022f42a38c195761457cedd058d339
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-12-16 02:55:21 -0500

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Filter from './components/Filter'
+import NavBar from './components/NavBar';
+import ListingContainer from './containers/ListingContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <>
+        <NavBar />
+        <main>
+          <Filter />
+          <ListingContainer />
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/containers/ListingContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+const ListingContainer = (props) => {
+
+  
+  return (
+    <>
+      <h1>ListingContainer</h1>
+    </>
+  )
+}
+
+export default ListingContainer;
